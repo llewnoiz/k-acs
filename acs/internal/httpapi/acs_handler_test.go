508456183052dd659724842e1bf4ddb16c5af510
hyunmin.song <hyunmin.song@bespinglobal.com>
2026-07-06 11:17:23 +0900

    test: add unit tests for parseUpTime function

---
+++ acs/internal/httpapi/acs_handler_test.go
@@ -0,0 +1,23 @@
+package httpapi
+
+import "testing"
+
+func TestParseUpTime(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int
+	}{
+		{"", 0},
+		{"123", 123},
+		{"0", 0},
+	}
+
+	for _, test := range tests {
+		t.Run(test.input, func(t *testing.T) {
+			result := parseUpTime(test.input)
+			if result != test.expected {
+				t.Errorf("parseUpTime(%q) = %d; want %d", test.input, result, test.expected)
+			}
+		})
+	}
+}
